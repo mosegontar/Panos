8ad1ce5f76fef0ba75e78e86f00ab9b666a9bf8d
mosegontar <mosegontar@gmail.com>
2017-10-19 13:25:28 -0400

    Update Stack class with custom errors

--- ch1ex1_stack.rb
+++ ch1ex1_stack.rb
@@ -1,14 +1,29 @@
+class StackFullError < StandardError
+    def initialize(msg="The stack is full")
+        super
+    end
+end
+
+class StackEmptyError < StandardError
+    def initialize(msg="The stack is empty")
+        super
+    end
+end
+
 class Stack
+    attr_reader :contents
     def initialize(maxsize=100)
         @contents = Array.new
         @maxsize = maxsize
     end
 
     def push(item)
+        raise StackFullError if full?
         @contents.push item
     end
 
     def pop
+        raise StackEmptyError if empty?
         @contents.pop
     end
 
